4e27d7e508b9cf76825ab24026cba33f6f5e7607
Pavel Rudenko <pogorus@gmail.com>
2021-11-01 12:53:14 +0300

    Iterator class changed

--- country_iterator.py
+++ country_iterator.py
@@ -1,12 +1,20 @@
 import json
 import re
 
-with open('countries.json') as f:
-    data = json.load(f)
-
 class CountryIterator:
+    def __init__(self):
+        with open('countries.json') as f:
+            self.data = json.load(f)
+        self.country_list = [country['name']['common'] for country in reversed(self.data)]
+        self.country_wiki_page = ''
+
+    def write_to_file(self, country_wiki_page):
+        with open('country_wiki_page.txt', 'ab') as f:
+            f.write(country_wiki_page.encode('utf-8'))
+        with open('country_wiki_page.txt', 'a') as f:
+            f.write('\n')
+
     def __iter__(self):
-        self.country_list = [country['name']['common'] for country in reversed(data)]
         return self
 
     def __next__(self):
@@ -14,14 +22,8 @@ class CountryIterator:
             raise StopIteration
         country = self.country_list.pop()
         link = r'https://en.wikipedia.org/wiki/' + re.sub(r' ', '_', country)
-        country_wiki_page = f'{country} - {link}'
-
-        def write_to_file():
-            with open('country_wiki_page.txt', 'ab') as f:
-                f.write(country_wiki_page.encode('utf-8'))
-            with open('country_wiki_page.txt', 'a') as f:
-                f.write('\n')
-        return write_to_file()
+        self.country_wiki_page = f'{country} - {link}'
+        return self.write_to_file(self.country_wiki_page)
 
 for country in CountryIterator():
     pass
